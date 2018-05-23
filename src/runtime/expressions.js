1bda3e36db1533e7703812a954cb85a0c6675b3e
kingcwt <1845023762@qq.com>
2018-05-23 18:14:50 +0800

    2

---
+++ src/runtime/expressions.js
@@ -0,0 +1,35 @@
+var ls;
+(function (ls) {
+	ls.1212 = function() {
+		return {
+			"Math.random()*100": function() { return Math.random()*100 },
+			"AISprite58": function() { return AISprite58 },
+			"AISprite110": function() { return AISprite110 },
+			"AISprite55": function() { return AISprite55 },
+			"%22lessOrEqual%22": function() { return "lessOrEqual" },
+			"Math.random()*200": function() { return Math.random()*200 },
+			"%22gameOver%22": function() { return "gameOver" },
+			"bs1": function() { return bs1 },
+			"%E7%BA%A2%E7%BA%A2%E8%89%B2%E6%88%98%E6%9C%BA": function() { return 红红色战机 },
+			"%22tongguan%22": function() { return "tongguan" },
+			"AISprite52": function() { return AISprite52 },
+			"%22hp%22": function() { return "hp" },
+			"bs2": function() { return bs2 }
+		}
+	};
+	ls.BOSS = function() {
+		return {
+			"%221212%22": function() { return "1212" }
+		}
+	};
+	ls.tongguan = function() {
+		return {
+			"%221212%22": function() { return "1212" }
+		}
+	};
+	ls.gameOver = function() {
+		return {
+			"%22BOSS%22": function() { return "BOSS" }
+		}
+	};
+})(ls || (ls = {}));
